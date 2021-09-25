a742e36a505d4461aaa27dc166046e694c5ccb25
Matheus Araujo Xavier <matheusxavier9630@gmail.com>
2021-09-25 12:39:25 -0300

    Projeto Gps

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+  @ViewChild ('map') mapRef: ElementRef;
+
+  map: google.maps.Map;
+  minhaPosicao: google.maps.LatLng;
+
+  constructor(private geolocation: Geolocation) {}
+
+  ionViewWillEnter(){
+    this.exibirMapa();
+  }
+
+  exibirMapa(): void{
+
+  const posicao = new google.maps.LatLng(-24.063506281958894, -46.552671245744186);
+  const opcoes = {
+    center: posicao,
+    zoom: 10,
+    disableDefaultUI: false,
+  } ;
+
+  this.map = new google.maps.Map(this.mapRef.nativeElement, opcoes);
+
+  this.buscarPosicao();
+
+  }
+
+  buscarPosicao(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.minhaPosicao = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  }
+
+  irParaposicao(){
+    this.map.setCenter(this.minhaPosicao);
+    this.map.setZoom(15);
+
+    new google.maps.Marker({
+      position: this.minhaPosicao,
+      map:this.map,
+      title: 'Minha Posição',
+      animation: google.maps.Animation.BOUNCE,
+    });
+  }
+
 
 }
